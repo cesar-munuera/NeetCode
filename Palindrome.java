e10b6fdabb24d1c703707f7588fe7238e9d4ee57
Cesar <cesarmunuera@anzenengineering.com>
2025-07-24 15:13:45 +0200

    Palindrome in Java

---
+++ Palindrome.java
@@ -0,0 +1,24 @@
+public class Palindrome {
+
+    public static boolean isPalindrome (int num){
+
+        String num_str = Integer.toString(num);
+        int num_len = num_str.length();
+        int iters = num_len/2;
+
+        for(int i=0; i<iters; i++){
+
+            if (num_str.charAt(i) != num_str.charAt(num_len-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+
+        // A palindrome is read the same starting from left or right
+        System.out.println(isPalindrome(112));
+    }
+    
+}

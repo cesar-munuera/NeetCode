3ed9b366479562688e96180b315dde6c9ba1c459
Cesar <cesarmunuera@anzenengineering.com>
2025-07-25 09:29:55 +0200

    Updated the description

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -3,11 +3,24 @@ import java.util.HashSet;
 import java.util.Set;
 
 class ContainsDuplicate {
+
+    /* Given an integer array nums, return true if any value appears at least twice in the array, 
+     * and return false if every element is distinct.
+     * 
+     * Input: nums = [1,2,3,1]
+     * Output: true
+     * 
+     * Input: nums = [1,2,3,4]
+     * Output: false
+     */
+
     public static boolean containsDuplicate(int[] nums) {
 
         Set<Integer> set_numbers = new HashSet<>();
 
         for(int num : nums){
+
+            // If .add() operation doesnt  complete, means it already exists
             if(!set_numbers.add(num)){
                 return true;
             }
@@ -17,8 +30,6 @@ class ContainsDuplicate {
 
     public static void main(String[] args){
         int[] nums_array = {1, 2, 3, 4, 4};
-
-        // Checks if an array has duplicated numbers
         System.out.println(containsDuplicate(nums_array));
     }
 }
